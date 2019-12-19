2981cc33820977a00e2f4e870d04a09a120d3a1f
samigumus <abdullahsamigumus@gmail.com>
2019-12-19 17:10:36 -0500

    create project

---
+++ src/test/java/com/odoo/runners/CucumberRunner.java
@@ -0,0 +1,19 @@
+package com.odoo.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com/odoo/step_definitions",
+        dryRun = false,
+        tags = "@smoke_test",
+        plugin = {"html:target/default-cucumber-reports",
+                  "json:target/cucumber.json"
+        }
+)
+public class CucumberRunner {
+
+}
