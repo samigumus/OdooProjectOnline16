120a0d811cac5fb45fe51112e327202ccd782b75
Meryemeca <meryem.kus@gmail.com>
2020-01-05 19:20:41 -0600

    configuration file is changed

--- src/test/java/com/odoo/pages/BasePage.java
+++ src/test/java/com/odoo/pages/BasePage.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
