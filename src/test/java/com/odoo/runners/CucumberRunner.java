1db8c93ceaaf81ae8b4db4c58fc997f0e180afe4
samigumus <abdullahsamigumus@gmail.com>
2020-01-05 22:49:16 -0500

    new branch

--- src/test/java/com/odoo/runners/CucumberRunner.java
+++ src/test/java/com/odoo/runners/CucumberRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/odoo/step_definitions",
         dryRun = false,
-        tags = "@events",
+        tags = "",
         plugin = {"html:target/default-cucumber-reports",
                   "json:target/cucumber.json"
         }
