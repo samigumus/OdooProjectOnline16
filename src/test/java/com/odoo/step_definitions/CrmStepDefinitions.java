0db532367eeef9d4edf475ad37d8800179a58d21
Meryemeca <meryem.kus@gmail.com>
2020-01-08 18:27:28 -0600

    CrmPage Pipeline Column verification

---
+++ src/test/java/com/odoo/step_definitions/CrmStepDefinitions.java
@@ -0,0 +1,45 @@
+package com.odoo.step_definitions;
+
+
+
+import com.odoo.pages.CrmPage;
+
+import com.odoo.utilities.BrowserUtils;
+
+import io.cucumber.java.en.Then;
+
+import org.junit.Assert;
+
+
+import java.util.List;
+
+
+public class CrmStepDefinitions {
+
+
+    CrmPage crm_page = new CrmPage();
+
+
+//    @Then("user navigates to {string} module")
+//    public void user_navigates_to_module(String moduleName) {
+//        crm_page.navigateTo(moduleName);
+//        System.out.println("You are on " + moduleName + " module!");
+//        BrowserUtils.wait(2);
+//
+//    }
+
+
+
+    @Then("user verifies, that columns name columnNames are display")
+    public void user_verifies_that_columns_name_columnNames_are_display(List<String> dataTable) {
+
+        BrowserUtils.wait(3);
+
+        Assert.assertEquals(dataTable, crm_page.getColumnNames(crm_page.columnNames));
+
+    }
+
+}
+
+
+
